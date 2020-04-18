580355c19d2b813812f1c35be8d7b020aeb68cda
Daria <fordaryapopova@yandex.ru>
2020-04-18 15:26:31 +0300

    Добавляет аккордеон

--- build/js/script.js
+++ build/js/script.js
@@ -1,13 +1,22 @@
-(function () {
-  'use strict';
+'use strict';
 
-  (() => {
-    const buttonElement = document.querySelector(`.page-header__button-consultation`);
-    const mobileWidth = 768;
-    const buttonText = `Бесплатная консультация`;
+(() => {
+  const buttonElement = document.querySelector(`.page-header__button-consultation`);
+  const mobileWidth = 768;
+  const buttonText = `Бесплатная консультация`;
 
-    if (buttonElement && screen.width < mobileWidth) {
-      buttonElement.textContent = buttonText;
+  if (buttonElement && screen.width < mobileWidth) {
+    buttonElement.textContent = buttonText;
+  }
+})();
+
+(() => {
+  const accordionElements = document.querySelectorAll(".accordion");
+
+  accordionElements.forEach(function(item) {
+    item.onclick = function(){
+      this.classList.toggle("accordion--close");
+      this.nextElementSibling.classList.toggle("accordion__content--show");
     }
-  })();
+  });
 })();
