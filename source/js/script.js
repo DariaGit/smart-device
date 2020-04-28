c890b076b5840dbd91be01518a0351323f758628
Daria <fordaryapopova@yandex.ru>
2020-04-28 20:39:07 +0300

    Заменяет скрипт плавного скроллинга

--- source/js/script.js
+++ source/js/script.js
@@ -121,11 +121,44 @@ $(document).mouseup(function (e) {
   }
 });
 
-$('body').on('click', '[href*="#"]', function (e) {
-  var FIXED_OFFSET = 100;
-  $('html,body').stop().animate({scrollTop: $(this.hash).offset().top - FIXED_OFFSET}, 1000);
-  e.preventDefault();
-});
+// Select all links with hashes
+$(function () {
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+}());
 
 $(function () {
   $('input[type="tel"]').mask('+7(000)000-00-00');
